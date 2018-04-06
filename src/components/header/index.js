f62deebba11429a91fb70e8c732629ac6215dee9
kaw28 <kaw28@njit.edu>
2018-04-05 22:45:15 -0400

    Initial Install

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+
+    render() {
+        return (
+            <h1>ToDo Application</h1>
+
+        );
+    }
+
+
+
+}
